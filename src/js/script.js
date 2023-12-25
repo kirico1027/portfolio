bfe37774bfe09222348045b0ed6606ec8f21676a
kirico <kirico1027@icloud.com>
2023-12-26 00:07:40 +0900

    top動作完了

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,25 @@ jQuery(function ($) {
 });
 
 
+$(function () {
+  // ウィンドウをスクロールしたら…
+  $(window).scroll(function () {
+    // ウィンドウの高さを取得
+    const wHeight = $(window).height();
+    // スクロールした量を取得
+    const wScroll = $(window).scrollTop();
+    // それぞれのblockクラスに対して…
+    $(".section-title, .category-list, .works-item").each(function () {
+      // それぞれのblockクラスのウィンドウからの高さを取得
+      const bPosition = $(this).offset().top;
+      // スクロールした量が要素の高さを上回ったら
+      // その数値にウィンドウの高さを引き、最後に200pxを足す
+      if (wScroll > bPosition - wHeight + 200) {
+        $(this).addClass("fadeIn");
+      }
+    });
+  });
+});
+
+
 });
